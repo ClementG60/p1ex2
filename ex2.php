58585a1adb41629448948c4e64aa57aa56aadd22
ClementG60 <cle_guillaume@hotmail.fr>
2021-12-07 10:50:42 +0100

    ajout code

---
+++ ex2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="ex2.css">
+    <title>Exercice2</title>
+</head>
+<body>
+    <?php
+    $firstname = 'Clément';
+    $lastname = 'Guillaume';
+    $age = 26;
+    echo '<p class="colorSentence">Bonjour ! Mon nom est ' .$firstname. ' ' .$lastname.' et mon âge est de ' .$age. 'ans.</p>';
+    ?>
+</body>
+</html>
